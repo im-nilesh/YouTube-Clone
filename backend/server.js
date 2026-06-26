8d7ade80a84a6337acf0b490d31936c48508399b
im-nilesh <nileshdadab@gmail.com>
2026-06-26 14:01:12 +0530

    implement user routes and update server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
 import express from "express";
 import "./database/db.js";
-import router from "./routes/routes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 const port = 5100;
 
 app.use(express.json());
+app.use("/", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
-
-app.get("/", (req, res) => {
-  res.send("Server is Running");
 });

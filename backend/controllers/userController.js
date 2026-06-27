5598349482e98b59b5cf0462b8144266a04991df
im-nilesh <nileshdadab@gmail.com>
2026-06-27 17:10:04 +0530

    refactor registerUser function to improve error handling and response structure

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,21 @@
 import User from "../models/User.js";
-
+import bcrypt from "bcrypt";
 async function registerUser(req, res) {
   try {
     const userData = req.body;
+    const existingUser = await User.findOne({
+      email: userData.email,
+    });
+    if (existingUser) {
+      return res.status(409).json({
+        message: "Email already exists",
+      });
+    }
+    userData.password = await bcrypt.hash(userData.password, 10);
     const user = new User(userData);
     await user.save();
-    res.json({
+
+    res.status(201).json({
       message: "User registered Successfully",
       user: {
         username: user.username,
@@ -13,7 +23,10 @@ async function registerUser(req, res) {
       },
     });
   } catch (error) {
-    res.json({ message: "An error occured" });
+    // res.json({ message: "An error occured" });
+    res.json({
+      message: error,
+    });
   }
 }
 export default registerUser;

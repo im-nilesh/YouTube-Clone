c3b26a6d4f20f4c39930fa72d51ab3285fec686c
im-nilesh <nileshdadab@gmail.com>
2026-06-27 13:54:54 +0530

    refactor registerUser function to include error handling and response structure

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,19 @@
 import User from "../models/User.js";
 
 async function registerUser(req, res) {
-  const userData = req.body;
-  const user = new User(userData);
-  await user.save();
+  try {
+    const userData = req.body;
+    const user = new User(userData);
+    await user.save();
+    res.json({
+      message: "User registered Successfully",
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.json({ message: "An error occured" });
+  }
 }
 export default registerUser;

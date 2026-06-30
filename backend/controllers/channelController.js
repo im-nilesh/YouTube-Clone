993b5d450400eb07c06b4be7b92d0d38d6e9e402
im-nilesh <nileshdadab@gmail.com>
2026-06-30 12:44:24 +0530

    fix: update import statements for consistency and clarity

--- backend/controllers/channelController.js
+++ backend/controllers/channelController.js
@@ -1,4 +1,4 @@
-import Channel from "../models/Channel";
+import Channel from "../models/Channel.js";
 
 async function createChannel(req, res) {
   try {
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import "./database/db.js";
-import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./database/db.js";
+
+import userRoutes from "./routes/userRoutes.js";
+import channelRoutes from "./routes/channelRoutes.js";
 
 const app = express();
 connectDB();
 
 app.use(express.json());
 app.use("/", userRoutes);
+app.use("/", channelRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);

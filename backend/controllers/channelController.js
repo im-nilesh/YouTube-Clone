f70a79cd7b5b4105f5357e8453a38a2dbe7e101f
im-nilesh <nileshdadab@gmail.com>
2026-07-02 11:08:26 +0530

    feat: refactor createChannel function to export and add getChannel function

--- backend/controllers/channelController.js
+++ backend/controllers/channelController.js
@@ -1,9 +1,16 @@
 import Channel from "../models/Channel.js";
 
-async function createChannel(req, res) {
+export async function createChannel(req, res) {
   try {
     const data = req.body;
-
+    const existingChannel = await Channel.findOne({
+      owner: req.user.id,
+    });
+    if (existingChannel) {
+      return res.status(409).json({
+        message: "Channel already exists",
+      });
+    }
     data.owner = req.user.id;
     const channel = new Channel(data);
     await channel.save();
@@ -17,4 +24,22 @@ async function createChannel(req, res) {
     });
   }
 }
-export default createChannel;
+
+export async function getChannel(req, res) {
+  try {
+    const id = req.params.id;
+    const channel = await Channel.findById(id);
+    if (!channel) {
+      return res.status(404).json({
+        message: "Channel not found",
+      });
+    }
+    return res.status(200).json({
+      channel,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}

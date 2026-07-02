b4a88d61994a7c6825414150e92f439fd7b97030
im-nilesh <nileshdadab@gmail.com>
2026-07-02 12:06:19 +0530

    feat: add getAllVideos and getVideo functions to videoController

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -24,3 +24,36 @@ export async function uploadVideo(req, res) {
     });
   }
 }
+
+export async function getAllVideos(req, res) {
+  try {
+    const videos = await Video.find();
+
+    return res.status(200).json({
+      videos,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}
+
+export async function getVideo(req, res) {
+  try {
+    const id = req.params.id;
+    const video = await Video.findById(id);
+    if (!video) {
+      return res.status(404).json({
+        message: "Video not found",
+      });
+    }
+    return res.status(200).json({
+      video,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}

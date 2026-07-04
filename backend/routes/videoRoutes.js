1319d39cb9ae906187cf758728bcbbe18ea59f8c
im-nilesh <nileshdadab@gmail.com>
2026-07-04 17:29:25 +0530

    feat: add video routes for managing video uploads and retrieval

--- backend/routes/videoRoutes.js
+++ backend/routes/videoRoutes.js
@@ -1,9 +1,24 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { uploadVideo } from "../controllers/videoController.js";
+
+import {
+  uploadVideo,
+  getAllVideos,
+  getVideo,
+  updateVideo,
+  deleteVideo,
+} from "../controllers/videoController.js";
 
 const router = express.Router();
 
 router.post("/video", authMiddleware, uploadVideo);
+
+router.get("/videos", getAllVideos);
+
+router.get("/video/:id", getVideo);
+
+router.put("/video/:id", authMiddleware, updateVideo);
+
+router.delete("/video/:id", authMiddleware, deleteVideo);
 
 export default router;

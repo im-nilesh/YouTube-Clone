132d7bb1314db21feb5404a859b9d35237119d8b
im-nilesh <nileshdadab@gmail.com>
2026-07-04 12:17:10 +0530

    feat: implement deleteComment function in commentController

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -64,7 +64,39 @@ export async function updateComment(req, res) {
     });
     return res.status(200).json({
       message: "Comment Updated successfully",
-      updatedComment,
+      comment: updatedComment,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}
+
+export async function deleteComment(req, res) {
+  try {
+    const commentId = req.params.id;
+    const userId = req.user.id;
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      return res.status(404).json({
+        message: "Comment not found",
+      });
+    }
+
+    if (comment.user.toString() !== userId) {
+      return res.status(403).json({
+        message: "User not authorized",
+      });
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    return res.status(200).json({
+      message: "Comment Deleted Successfully",
+      comment: deletedComment,
     });
   } catch (error) {
     return res.status(500).json({

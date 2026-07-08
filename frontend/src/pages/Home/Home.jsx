bc58724ef30942c8f830e06a66586284e2ebd5c7
im-nilesh <nileshdadab@gmail.com>
2026-07-08 19:06:02 +0530

    feat: add FilterBar component to enhance video filtering functionality

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import { useSearchParams } from "react-router-dom";
 
 import "./Home.css";
 
-import FilterBar from "../../components/FilterBar/FilterBar"; //updated
+import FilterBar from "../../components/FilterBar/TempFilterBar"; //updated
 import VideoCard from "../../components/VideoCard/VideoCard";
 import { useVideo } from "../../context/VideoContext";
 import {
